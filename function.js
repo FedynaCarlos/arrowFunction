f05820297fe7a228e1acea697b54e72c61f8cff4
FedynaCarlos <pacofedyna@gmail.com>
2022-09-23 17:52:31 -0300

    arrow

---
+++ function.js
@@ -0,0 +1,11 @@
+//function normal
+function saludo(algo) {
+	return algo;
+}
+console.log(saludo('Hola'));
+
+//arrow function
+let nombre2 = (a, b) => {
+	return a + b + 6;
+};
+console.log(nombre2(98, 1));
